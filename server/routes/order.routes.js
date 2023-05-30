958f6a330603dd305af7787735e12835190d35f6
Ivan Popovych <ipopovych2004@gmail.com>
2023-05-30 11:32:09 +0300

    separete server from client

--- server/routes/order.routes.js
+++
@@ -1,7 +0,0 @@
-const Router = require('express')
-const OrderController = require('../controllers/order.controller')
-const router = new Router()
-
-router.post('/', OrderController.createOrder)
-
-module.exports = router
--- server/routes/product.route.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const ProductController = require('../controllers/product.controller')
-const router = new Router()
-
-router.get('/', ProductController.getAllProducts)
-router.post('/', ProductController.createProduct)
-router.get('/:shopId', ProductController.getAllProductsByShop)
-
-module.exports = router
